cae33efd6673e650adf3fa37011417b4eeac580f
CDAMIAN <cdamian@EDCMXENLT0599.local>
2021-08-11 14:12:27 -0500

    add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CoinDetailPage from "./pages/CoinDetailPage";
+import CoinSummaryPage from "./pages/CoinSummaryPage";
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+          <Route exact path='/' component={CoinSummaryPage}/>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
